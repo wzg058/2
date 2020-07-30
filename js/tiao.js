5a286e376a6cd80ee36b979347ddf7ddadf3d84a
wangzhigang <guaika.w@qq.com>
2020-07-30 14:19:22 +0800

    '提交'

---
+++ js/tiao.js
@@ -0,0 +1,9 @@
+$('.lis>li:eq(0)').on('touchstart',function(){
+    location.href = './common.html';
+});
+$('.top>div>img:eq(0)').on('touchstart',function(){
+    location.href = './my.html';
+});
+$('.home-list>li:eq(0)').on('touchstart',function(){
+    location.href = './details.html';
+});
